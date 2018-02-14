d337259c381defb3ff7a2da14debe0ea3bfd230b
Simon Marechal <bartavelle@gmail.com>
2018-02-14 19:25:03 +0100

    Drop hspec-discover so that travis works

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,15 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import qualified Elm.DeriveSpec
+import qualified Elm.TyRenderSpec
+import qualified Elm.JsonSpec
+import qualified Elm.ModuleSpec
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Elm.DeriveSpec" Elm.DeriveSpec.spec
+  describe "Elm.TyRenderSpec" Elm.TyRenderSpec.spec
+  describe "Elm.JsonSpec" Elm.JsonSpec.spec
+  describe "Elm.ModuleSpec" Elm.ModuleSpec.spec
